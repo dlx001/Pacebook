5ef6df1bb966c877972384c1db7c8d1f15e3bf07
DX <danielxu.314@gmail.com>
2022-12-21 14:32:36 -0800

    add documentation

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+//create an express app
 const express = require("express");
 const app=express();
 
+//use ejs for view engine
 app.set('view engine', 'ejs');
 
+//listen on localhost 3000
 app.listen(3000);
 
+//create public files to href css styles
+app.use(express.static('public'))
+
+//links
 app.get('/', (req,res)=>{
     res.render('index');
 })
